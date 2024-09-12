8eb66d1ee271cfd520a9db0aebc1ae882c9d156c
gregmakernexus <greg.yamanishi@makernexus.org>
2024-09-12 09:30:05 -0700

    Update daily.sh to not poll at night

--- Software/Report_Creator_Software/daily_log/daily.sh
+++ Software/Report_Creator_Software/daily_log/daily.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-cd /$HOME/Documents/Online_Visitor_Log/Software/Report_Creator_Software/daily_log
+function run_daily_report {
+  cd /$HOME/Online_Visitor_Log/Software/Report_Creator_Software/daily_report
+  ./daily_report
+}
+
 while true; do
-  ./daily_log
-  sleep 300
+  hour=`date "+%H"`
+  if [[ $hour >= "01" && $hour < "08" ]]; then
+    echo 'the hour is' $hour
+    sleep 60m
+  else
+    run_daily_report
+    sleep 5m
+  fi
 done
